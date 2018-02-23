367804e178bc02012bbdaf963fc7a255ff5e6915
Der Flo <derflo.dev@gmail.com>
2018-02-23 10:28:32 +0100

    Update initialize.sh

--- node/initialize.sh
+++ node/initialize.sh
@@ -2,7 +2,8 @@
 
 # Update
 apt-get update
-apt-get install -y htop vim python3 python3-requests bluetooth python-bluez git pkg-config libboost-python-dev libboost-thread-dev libbluetooth-dev libglib2.0-dev python-dev mercurial 
+apt-get install -y htop vim python3 python3-requests bluetooth python-bluez git 
+apt-get install pkg-config libboost-python-dev libboost-thread-dev libbluetooth-dev libglib2.0-dev python-dev mercurial 
 
 # Install wireshark and other basics using all defaults
 DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy install wireless-tools firmware-atheros usbutils wireshark tshark hostapd
@@ -13,6 +14,8 @@ sudo python setup.py install
 
 # Copy latest scan.py from the repo
 wget https://raw.githubusercontent.com/derfloDev/find-lf/master/node/scan.py -O scan.py
+sudo chown pi scan.py
+sudo chgrp pi scan.py
 
 git clone https://github.com/derfloDev/bluetooth-proximity.git
 cd bluetooth-proximity
